922152b48fe145216a74aed68eb85b6453ad0f6a
Koshal Parwan <parwan.kp@pg.com>
2022-12-12 22:28:06 +0400

    header

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Avatar, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+import './Header.css';
+import { ArrowDropDown } from '@mui/icons-material';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header__left">
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+
+                <img
+                    src='https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png'
+                    alt='mailbox'
+                />
+            </div>
+
+            <div className="header__middle">
+                <SearchIcon />
+                <input placeholder='Search mail' type="text" id="searchbox" name="search-box" aria-label="search"/>
+                <ArrowDropDown className='header__inputCaret'/>
+            </div>
+
+            <div className="header__right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                <Avatar />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
