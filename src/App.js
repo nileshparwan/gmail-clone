9eeadf51caf1bd63adfc81a8426502695014beac
Koshal Parwan <parwan.kp@pg.com>
2022-12-12 23:42:18 +0400

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 import './App.css';
 
@@ -7,6 +8,10 @@ function App() {
   return (
     <div className="App">
       <Header />
+
+      <aside>
+        <Sidebar />
+      </aside>
     </div>
   );
 }
