24643ad08679d858ea11f0858a2d700be3efc52a
Koshal Parwan <parwan.kp@pg.com>
2022-12-14 19:49:16 +0400

    email list and row + fix sidebar hover

--- src/components/Mail.js
+++ src/components/Mail.js
@@ -1,12 +1,80 @@
-import React from 'react'
-import './Mail.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { IconButton } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import MoveToInboxIcon from '@mui/icons-material/MoveToInbox';
+import ErrorIcon from '@mui/icons-material/Error';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EmailIcon from '@mui/icons-material/Email';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
+import PrintIcon from '@mui/icons-material/Print';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import LabelImportantIcon from '@mui/icons-material/LabelImportant';
+
+import './Mail.css';
 
 const Mail = () => {
-  return (
-    <div>
-        <h1>mail</h1>
-    </div>
-  )
-}
+    const history = useNavigate();
 
-export default Mail
+    return (
+        <div className='mail'>
+            <div className="mail__tools">
+                <div className="mail__toolsleft">
+                    <IconButton onClick={() => history("/")}>
+                        <ArrowBackIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <MoveToInboxIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <ErrorIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <DeleteIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <EmailIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <WatchLaterIcon />
+                    </IconButton>
+                </div>
+
+                <div className="mail__toolsRight">
+                    <IconButton>
+                        <UnfoldMoreIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <PrintIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <ExitToAppIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="mail__body">
+                <div className="mail__bodyHeader">
+                    <h2>Subject</h2>
+                    <LabelImportantIcon className="mail__important" />
+                    <p>title</p>
+                    <p className='mail__time'>10pm</p>
+                </div>
+
+                <div className='mail__message'>
+                    <p>this is a message</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Mail;
